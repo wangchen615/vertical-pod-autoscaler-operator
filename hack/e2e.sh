a2df53ff3b7179cf4ad67187722b6421fc0c83b1
Chen-Wang1 <Chen.Wang1@ibm.com>
2021-03-17 13:20:13 -0400

    add Makefile target e2e-olm-local e2e-local e2e-olm-ci e2e-ci to automate the olm installation and the upstream e2e tests

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -4,9 +4,37 @@ set -euo pipefail
 
 GOPATH="$(mktemp -d)"
 export GOPATH
+AUTOSCALER_PKG="k8s.io/autoscaler"
+echo "Get the k8s.io/autoscaler package!"
+GO111MODULE=auto go get -u -d "${AUTOSCALER_PKG}/..."
 
-ACTUATOR_PKG="github.com/openshift/cluster-api-actuator-pkg"
+echo "Check the VerticalPodAutoScalerController configurations ..."
+recommendationOnly=$(kubectl get VerticalPodAutoScalerController default -n openshift-vertical-pod-autoscaler -oyaml|yq ".spec.recommendationOnly" || false)
 
-go get -u -d "${ACTUATOR_PKG}/..."
+SCRIPT_ROOT=${GOPATH}/src/${AUTOSCALER_PKG}/vertical-pod-autoscaler/
+if $recommendationOnly
+then
+  echo "recommendationOnly is enabled. Run the recommender e2e tests in upstream ..."
+  pushd ${SCRIPT_ROOT}/e2e
+  GO111MODULE=on go test -mod vendor ./v1/*go -v --test.timeout=60m --args --ginkgo.v=true --ginkgo.focus="\[VPA\] \[recommender\]" --ginkgo.skip="doesn't drop lower/upper after recommender's restart" --report-dir=/workspace/_artifacts --disable-log-dump
+  V1_RESULT=$?
+  popd
+  echo "v1 recommender test result:" ${V1_RESULT}
 
-exec make --directory="${GOPATH}/src/${ACTUATOR_PKG}" test-e2e
+  if [ $V1_RESULT -gt 0 ]; then
+    echo "Tests failed"
+    exit 1
+  fi
+else
+  echo "recommendationOnly is disabled. Run the full-vpa e2e tests in upstream ..."
+  pushd ${SCRIPT_ROOT}/e2e
+  GO111MODULE=on go test -mod vendor ./v1/*go -v --test.timeout=60m --args --ginkgo.v=true --ginkgo.focus="\[VPA\] \[full-vpa\]" --report-dir=/workspace/_artifacts --disable-log-dump
+  V1_RESULT=$?
+  popd
+  echo "v1 full-vpa test result:" ${V1_RESULT}
+
+  if [ $V1_RESULT -gt 0 ]; then
+    echo "Tests failed"
+    exit 1
+  fi
+fi
